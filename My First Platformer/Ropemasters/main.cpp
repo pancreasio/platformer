c545dce8eb591a9448d376ccd09cab442895a40b
unknown <pehuen-testa@hotmail.com>
2018-11-09 12:31:53 -0300

    first commit

---
+++ My First Platformer/Ropemasters/main.cpp
@@ -0,0 +1,33 @@
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Audio.hpp>
+#include <STP\TMXLoader.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(360, 280), "STP Example");
+	tmx::TileMap map("res/caveTM.tmx");
+
+	map.ShowObjects(); // Display all the layer objects.
+
+	//map.GetLayer("World").visible = false; // Hide a Layer named World
+
+										   // Start the game loop
+	while (window.isOpen()) {
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			// Close window : exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+		// Draw the map
+		window.draw(map);
+		// Update the window
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
+}
