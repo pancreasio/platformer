c9a9d490f202f1fb02e098e1fdb54abe197326c2
pancreasio <julianholisu@gmail.com>
2018-11-13 09:47:09 -0300

    gitignore toma los dll

--- My First Platformer/res/castleTS.tsx
+++ My First Platformer/res/castleTS.tsx
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="castleTS" tilewidth="70" tileheight="70" tilecount="49" columns="7">
- <image source="castleTS.png" width="512" height="512"/>
-</tileset>
+version https://git-lfs.github.com/spec/v1
+oid sha256:5bd6c3a2073b581095a29a4e29de889b51af5c1a7f892e1df958af0cd1413d39
+size 230
